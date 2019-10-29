151143c3cdfd23bcac6e45876c4a21b71f08b9f2
ezequiel oyhambure <ezeoyhambure@gmail.com>
2019-10-29 09:27:23 -0300

    SOY DIOS PUTOS

---
+++ Code/strataGEM/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace strataGEM.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<strataGEM.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "strataGEM.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(strataGEM.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
